57515358f369b65ad1ce5425a7ca3f5f1f5ebc55
KeyVault Deploy <deploy@keyvault.app>
2026-07-06 16:40:18 +0800

    chore: 清理已删除文件的 git 索引

--- _upgrade_final.py
+++
@@ -1,2 +0,0 @@
-import re
-p=r" C:\\Users\\31961\\Local Sites\\keysavings\\app\\public\\wp-content\\themes\\astra\\assets\\js\\b2b-component-plain.js\nprint(\script ready\)
--- start.ps1
+++
@@ -1,1 +0,0 @@
-cmd /c start /b node "C:\Users\31961\Documents\microsoft web\server.js" > "C:\Users\31961\Documents\microsoft web\server.log" 2>&1
